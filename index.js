d5f60aaf6ecd3cc9b78f51d097f3f49e8c29587b
nigahiga34 <kingoc39@gmail.com>
2024-08-28 12:19:03 +0100

    idk

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ const __dirname = dirname(__filename)
 const app = express()
 const port = process.env.PORT || 3000
 
-// Serve static files from the 'public' directory
+
 app.use(express.static(join(__dirname, 'public')))
 
-// Catch-all route to serve index.html for any unmatched routes
 app.get('*', (req, res) => {
  res.sendFile(join(__dirname, 'public', 'index.html'))
 })
